73274fcf3871e85a5e9a3d3edf9a2c648ae81084
slepimis120 <slepimis120@gmail.com>
2024-01-30 23:20:41 +0100

    feat: implement print for accuracy, precision, recall and F1

--- SVM.py
+++ SVM.py
@@ -2,12 +2,33 @@ import os
 
 import joblib
 import librosa
+import contextlib
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 
 import numpy as np
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+def print_metrics(x_val, y_val, best_model):
+    # Make predictions on the validation set
+    y_val_pred = best_model.predict(x_val)
+
+    # Calculate precision, recall, and F1 score
+    precision = precision_score(y_val, y_val_pred, average='weighted', zero_division=1)
+    recall = recall_score(y_val, y_val_pred, average='weighted', zero_division=1)
+    f1 = f1_score(y_val, y_val_pred, average='weighted')
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y_val, y_val_pred)
+
+    # Print the accuracy, precision, recall, and F1 score
+    print(f'Accuracy: {accuracy:.4f}')
+    print(f'Precision: {precision:.4f}')
+    print(f'Recall: {recall:.4f}')
+    print(f'F1 Score: {f1:.4f}')
 
 
 def read_data(path):
@@ -16,7 +37,8 @@ def read_data(path):
     labels = []
 
     for f in audio_files:
-        audio, sample_rate = librosa.load(os.path.join(path, f))
+        with contextlib.redirect_stderr(None):
+            audio, sample_rate = librosa.load(os.path.join(path, f))
         mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
         mfcc = np.mean(mfcc.T, axis=0)
         features.append(mfcc)
@@ -45,6 +67,4 @@ def get_audio_model(path):
     model = train_svm(X_train, y_train)
     joblib.dump(model, './data/model/svm_model.h5')
 
-    y_pred_train = model.predict(X_train)
-
-    y_pred_test = model.predict(X_test)
+    print_metrics(X_test, y_test, model)
